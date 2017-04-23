89294754e8d0c31a87f51b9eb2934c1938829afd
whitecell <whitecellcisco@gmail.com>
2017-04-22 22:38:18 -0400

    cmit2 Revert "cmit2"
    This reverts commit bc299be4b34e5e4656bbe559aa67ad9fa9104a68.
    
    Revert "Revert "cmit2""
    
    This reverts commit 12c538f8f3df059832ed73be11cb8db44632b35b.

--- src/example/nosql/ResourceServlet.java
+++ src/example/nosql/ResourceServlet.java
@@ -41,6 +41,9 @@ public class ResourceServlet {
 			throws Exception {
 
 		Database db = null;
+		
+		
+		Database db2 = null;
 		try {
 			db = getDB();
 		} catch (Exception re) {
@@ -51,7 +54,9 @@ public class ResourceServlet {
 		String idString = id == null ? null : id.toString();
 		JsonObject resultObject = create(db, idString, name, value, null, null);
 
+
 		System.out.println("Create Successful.45678");
+
 
 		return Response.ok(resultObject.toString()).build();
 	}
